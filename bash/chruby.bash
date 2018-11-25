ae886009dd9e265dd23d1e78217fbfcd0952578c
Andre Arko <andre@arko.net>
2018-11-24 21:26:13 -0800

    [chruby] set up chruby from homebrew, not boxen

--- bash/chruby.bash
+++ bash/chruby.bash
@@ -1,25 +1,26 @@
 function reload_chruby {
   # base chruby function, aliased to original_chruby
-  if [[ -f /opt/boxen/chruby/share/chruby/chruby.sh ]]; then
-    source /opt/boxen/chruby/share/chruby/chruby.sh
+  if [[ -f /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
+    source /usr/local/opt/chruby/share/chruby/chruby.sh
     eval "original_$(declare -f chruby)"
-  fi
 
-  # chruby wrapper that includes reloading available rubies
-  function chruby {
-    if [[ $1 == "reload" ]]; then
-      reload_chruby
-    else
-      original_chruby "$@"
-    fi
-  }
+    # chruby wrapper that includes reloading available rubies
+    function chruby {
+      if [[ $1 == "reload" ]]; then
+        reload_chruby
+      else
+        original_chruby "$@"
+      fi
+    }
+
+  fi
 }
 
 reload_chruby
 
 # chruby .ruby-version support
-if [[ -f /opt/boxen/chruby/share/chruby/auto.sh ]]; then
-  source /opt/boxen/chruby/share/chruby/auto.sh
+if [[ -f /usr/local/opt/chruby/share/chruby/auto.sh ]]; then
+  source /usr/local/opt/chruby/share/chruby/auto.sh
 fi
 
 # chruby tab completion
