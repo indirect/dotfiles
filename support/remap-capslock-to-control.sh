f4d992e7efe129a6da5ace0becf7395bc208f89c
Andre Arko <andre@arko.net>
2020-07-31 21:01:54 -0700

    [Keyboard] update remap for 2016+ SPI keyboards
    also, apply changes immediately, without restarting! woohoo

--- support/remap-capslock-to-control.sh
+++ support/remap-capslock-to-control.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+set -vx
+
 # Remap capslock to control. Really.
 #
 # list of keyboards plugged in to this computer
-keyboard_ids=$(ioreg -c AppleHSSPIInterface -r | grep -E 'idVendor|idProduct' | awk '{print $4}' | paste -s -d'-\n' -)
-echo $keyboard_ids
+keyboard_ids=$(ioreg -c AppleHSSPIInterface -r | grep -E 'idVendor|idProduct' | awk '{print $4}' | tac | paste -s -d '-\n' - | sort | uniq)
 
 # check if the keyboards are already remapped
-echo $keyboard_ids | xargs -I{} sh -c 'defaults -currentHost read -g "com.apple.keyboard.modifiermapping.{}-0" | grep "Dst = 2" > /dev/null'
+echo $keyboard_ids | xargs -I{} sh -c 'defaults -currentHost read -g "com.apple.keyboard.modifiermapping.{}-0" | grep "Dst = 30064771300" > /dev/null'
 
-exit 0
 if [[ $? -ne 0 ]]; then
   # remap the keyboards
-  echo $keyboard_ids | xargs -I{} defaults -currentHost write -g "com.apple.keyboard.modifiermapping.{}-0" -array "<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>"
+  echo $keyboard_ids | xargs -I{} defaults -currentHost write -g "com.apple.keyboard.modifiermapping.{}-0" -array "<dict><key>HIDKeyboardModifierMappingDst</key><integer>30064771300</integer><key>HIDKeyboardModifierMappingSrc</key><integer>30064771129</integer></dict>"
+fi
+
+mapping=$(hidutil property --get "UserKeyMapping")
+if [[ "$mapping" == "(null)" ]]; then
+  hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0}]}'
 fi
