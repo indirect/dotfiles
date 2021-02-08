7754eb734dbdf028e15e84a8ed0970605e31f555
Andre Arko <andre@arko.net>
2021-02-07 17:55:40 -0800

    [fzf] use fzf from BREW_PREFIX for M1

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,13 +1,7 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
-fi
-
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$BREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$BREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
