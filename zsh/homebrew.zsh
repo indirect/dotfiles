13148ea661da4ed00246563ddd222c5127c16f40
Andre Arko <andre@arko.net>
2021-02-19 00:48:59 -0800

    [zsh] support for onivim2

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -1,3 +1,5 @@
 # set up homebrew
 export BREW_PREFIX=$([[ "$(arch)" == "arm64" ]] && echo "/opt/homebrew" || echo "/usr/local")
 [[ "$PATH" != "*$BREW_PREFIX/bin*" ]] && export PATH="$BREW_PREFIX/bin:$PATH"
+
+export HOMEBREW_ONIVIM_SERIAL="$(security find-generic-password -s onivim2 -w)"
