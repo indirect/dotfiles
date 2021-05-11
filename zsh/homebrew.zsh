54c3d0efaaf480251a2b2ce4e17e87c52c1f54b8
Andre Arko <andre@arko.net>
2021-05-11 14:49:28 -0700

    [zsh] disable onivim key to speed up brew

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -2,4 +2,5 @@
 export BREW_PREFIX=$([[ "$(arch)" == "arm64" ]] && echo "/opt/homebrew" || echo "/usr/local")
 [[ "$PATH" != "*$BREW_PREFIX/bin*" ]] && export PATH="$BREW_PREFIX/bin:$PATH"
 
-export HOMEBREW_ONIVIM_SERIAL="$(security find-generic-password -s onivim2 -w)"
+# disable this because it makes homebrew very slow
+# export HOMEBREW_ONIVIM_SERIAL="$(security find-generic-password -s onivim2 -w)"
