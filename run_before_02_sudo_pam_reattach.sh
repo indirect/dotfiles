4bcae6746f81835bdcfd6c4d42d6512c3f5cf3bc
Andre Arko <andre@arko.net>
2022-05-23 01:42:04 -0700

    [chezmoi] return early if pam_reattach is set up

--- run_before_02_sudo_pam_reattach.sh
+++ run_before_02_sudo_pam_reattach.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-set -vx
+
+if grep -q pam_reattach.so /etc/pam.d/sudo; then
+  exit 0
+fi
 
 Q="-q"
 BREW_PREFIX="${BREW_PREFIX:-$(brew --prefix)}"
 PAM_REATTACH="$BREW_PREFIX/lib/pam/pam_reattach.so"
-test -e "$PAM_REATTACH" || brew install pam_reattach
-
-echo "Configuring sudo with pam_reattach:"
 PAM_FILE="/etc/pam.d/sudo"
 FIRST_LINE="# sudo: auth account password session"
+TOUCHID_LINE="auth       optional       $PAM_REATTACH"
+
+set -vx
+test -e "$PAM_REATTACH" || brew install pam_reattach
+echo "Configuring sudo with pam_reattach:"
 
 if grep $Q pam_reattach.so "$PAM_FILE"; then
   echo "Already set up"
 elif ! head -n1 "$PAM_FILE" | grep $Q "$FIRST_LINE"; then
   echo "$PAM_FILE is not in the expected format!"
 else
-  TOUCHID_LINE="auth       optional       $PAM_REATTACH"
   sudo sed -i.bak -e \
     "s|$FIRST_LINE|$FIRST_LINE\n$TOUCHID_LINE|" \
     "$PAM_FILE"
