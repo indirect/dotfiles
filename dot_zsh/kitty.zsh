f6d7ddbf356d2ab13a4e332bf789c12d88da347c
Andre Arko <andre@arko.net>
2022-05-28 19:15:26 -0700

    [zsh] fix kitty init error in tmux in Terminal.app

--- dot_zsh/kitty.zsh
+++ dot_zsh/kitty.zsh
@@ -1,8 +1,10 @@
 if test -n "$KITTY_INSTALLATION_DIR"; then
   alias ssh="kitty +kitten ssh"
 
-  export KITTY_SHELL_INTEGRATION="enabled"
-  autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
-  kitty-integration
-  unfunction kitty-integration
+  if test -d "$KITTY_INSTALLATION_DIR"; then
+    export KITTY_SHELL_INTEGRATION="enabled"
+    autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+    kitty-integration
+    unfunction kitty-integration
+  fi
 fi
