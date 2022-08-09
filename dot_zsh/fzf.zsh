521458c5dbdc99a0636f19fa21aee1b0596a7e56
Andre Arko <andre@arko.net>
2022-08-09 01:38:33 -0700

    [zsh] use znap to load fzf setup

--- dot_zsh/fzf.zsh
+++
@@ -1,7 +0,0 @@
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$BREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$BREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
