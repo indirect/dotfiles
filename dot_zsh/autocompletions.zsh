1fc5a02721835d1b51b43c1698098d4e728337d2
Andre Arko <andre@arko.net>
2022-08-09 01:39:53 -0700

    [zsh] autoload can take a glob

--- dot_zsh/autocompletions.zsh
+++
@@ -1,5 +0,0 @@
-# Load zsh autocompletions
-fpath+="$BREW_PREFIX/share/zsh/site-functions"
-fpath+=~/.zsh/plugins/mac-zsh-completions/completions
-fpath+=~/.zsh/completion
-autoload -Uz compinit && compinit
