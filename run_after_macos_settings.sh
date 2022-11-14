a26585720bc53f314674a04546e4de579b1e4aac
Andre Arko <andre@arko.net>
2022-11-14 02:10:40 -0800

    [macOS] disable Xcode Cloud upsell dialog

--- run_after_macos_settings.sh
+++ run_after_macos_settings.sh
@@ -43,3 +43,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# disable Xcode Cloud upsell
+defaults write com.apple.dt.Xcode XcodeCloudUpsellPromptEnabled -bool false
