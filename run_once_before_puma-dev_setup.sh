75ada718cce58c91f359057533607184ad1345bf
Andre Arko <andre@arko.net>
2023-09-18 22:21:54 -0700

    [puma-dev] update install arguments

--- run_once_before_puma-dev_setup.sh
+++ run_once_before_puma-dev_setup.sh
@@ -6,5 +6,5 @@ sudo puma-dev -setup
 {{ if .server }}
 puma-dev -install
 {{ else }}
-puma-dev -install -install-http-port 9280 -install-https-port 9283
+puma-dev -install -install-port 9280 -install-https-port 9283
 {{ end }}
