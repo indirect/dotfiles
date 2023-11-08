219ce17f6c5be351b67b0705a6e4ac44aab61921
Andre Arko <andre@arko.net>
2023-11-08 00:55:02 -0800

    [open_everything] update apps to open after

--- run_once_after_zz_open_everything.sh
+++ run_once_after_zz_open_everything.sh
@@ -8,15 +8,13 @@ if [[ ! -d /Library/PreferencePanes/BackblazeBackup.prefPane ]]; then
   open "$(echo $BREW_PREFIX/Caskroom/backblaze/*/*Installer.app)"
 fi
 
-open -a "Alfred 4"
-open -a "Bartender 4"
-open -a "Dropbox"
+open -a "Alfred 5"
+open -a "Bartender 5"
+open -a "Maestral"
 open -a "Due"
 open -a "Hammerspoon"
-open -a "Instapaper Save"
 open -a "Karabiner-Elements"
 open -a "Keyboard Maestro"
 open -a "Little Snitch"
-open -a "MenuMeters"
 open -a "Rectangle Pro"
 open -a "Tailscale"
