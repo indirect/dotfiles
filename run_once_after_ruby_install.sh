d37e2c39596200c573e9183bf2cb919fd5a4b362
Andre Arko <andre@arko.net>
2023-11-08 00:50:23 -0800

    [ruby] install rubies without bash chruby

--- run_once_after_ruby_install.sh
+++ run_once_after_ruby_install.sh
@@ -1,17 +1,12 @@
-#!/bin/bash
+#!/bin/zsh -li
 # set -euo pipefail
 set -vx
 
 # Read the default ruby version out of ~/.zshrc
-eval $(grep DEFAULT_RUBY_VERSION= zshrc)
-
-# Load chruby into this shell if necessary
-if [[ "$(type -w chruby)" != "chruby: function" ]]; then
-  source ~/.zsh/chruby.zsh
-fi
+eval "$(grep DEFAULT_RUBY_VERSION= ~/.zshrc)"
 
 # Install the current default Ruby version if necessary
 if [[ ! -d "$HOME/.rubies/ruby-$DEFAULT_RUBY_VERSION" ]]; then
-  chruby install "$DEFAULT_RUBY_VERSION"
+  chruby install "$DEFAULT_RUBY_VERSION" -- --with-openssl-dir="$(brew --prefix openssl@1.1)"
   chruby reload
 fi
