d21dcbb278dc96b8a5627d26a6d84e1e47036cca
Andre Arko <andre@arko.net>
2023-11-12 21:17:38 -0800

    [chezmoi] skip littlesnitch if not installed

--- run_once_after_zz_open_everything.sh
+++ run_once_after_zz_open_everything.sh
@@ -8,6 +8,8 @@ if [[ ! -d /Library/PreferencePanes/Backblaze\ Settings\ Launcher.prefPane ]]; t
   open "$(echo $BREW_PREFIX/Caskroom/backblaze/*/*Installer.app)"
 fi
 
+test -e "/Applications/Little Snitch.app" && open -a "Little Snitch"
+
 open -a "Alfred 5"
 open -a "Bartender 5"
 open -a "Maestral"
@@ -15,6 +17,5 @@ open -a "Due"
 open -a "Hammerspoon"
 open -a "Karabiner-Elements"
 open -a "Keyboard Maestro"
-open -a "Little Snitch"
 open -a "Rectangle Pro"
 open -a "Tailscale"
