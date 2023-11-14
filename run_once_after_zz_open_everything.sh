4aa462c4493a18734f3f52a5b26f3123e889e11e
Andre Arko <andre@arko.net>
2023-11-14 13:03:40 -0800

    [chezmoi] open applications only once

--- run_once_after_zz_open_everything.sh
+++ run_once_after_zz_open_everything.sh
@@ -4,18 +4,36 @@ set -vx
 
 BREW_PREFIX="${BREW_PREFIX:-$(brew --prefix)}"
 
-if [[ ! -d /Library/PreferencePanes/Backblaze\ Settings\ Launcher.prefPane ]]; then
-  open "$(echo $BREW_PREFIX/Caskroom/backblaze/*/*Installer.app)"
-fi
+open_application() {
+  local opened_dir="${HOME}/.local/state/chezmoi/opened"
+  mkdir -p "${opened_dir}"
+  local app_name="$1"
+  local touched_file="${opened_dir}/${app_name}"
+  local required_file="${2:-}"
 
-test -e "/Applications/Little Snitch.app" && open -a "Little Snitch"
+  # Check if the optional file path is provided and if the file exists
+  if [ -n "${required_file}" ] && [ ! -e "${required_file}" ]; then
+    echo "Skipping opening ${app_name} because the file at ${required_file} does not exist."
+  elif [ ! -e "/Applications/${app_name}.app" ]; then
+    echo "Skipping ${app_name} because it is not in /Applications."
+  elif [ -e "${touched_file}" ]; then
+    echo "${app_name} was already opened."
+  else
+    open -a "${app_name}"
+    touch "${touched_file}"
+    echo "Opened ${app_name} and touched ${touched_file}."
+  fi
+}
 
-open -a "Alfred 5"
-open -a "Bartender 5"
-open -a "Maestral"
-open -a "Due"
-open -a "Hammerspoon"
-open -a "Karabiner-Elements"
-open -a "Keyboard Maestro"
-open -a "Rectangle Pro"
-open -a "Tailscale"
+open_application "$(echo $BREW_PREFIX/Caskroom/backblaze/*/*Installer.app)", "/Library/PreferencePanes/Backblaze Settings Launcher.prefPane"
+open_application "Little Snitch", 
+open_application "Alfred 5"
+open_application "Bartender 5"
+open_application "Maestral"
+open_application "Due"
+open_application "Hammerspoon"
+open_application "Karabiner-Elements"
+open_application "Keyboard Maestro"
+open_application "Rectangle Pro"
+open_application "Tailscale"
+open_application "Xcode"
