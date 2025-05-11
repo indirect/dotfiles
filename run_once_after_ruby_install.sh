c6b3807291fc9c9d57d6bc79366b92966e2c9a7f
André Arko <andre@arko.net>
2025-05-11 14:42:32 -0700

    [chezmoi] fix ruby install to use latest

--- run_once_after_ruby_install.sh
+++ run_once_after_ruby_install.sh
@@ -1,12 +1,4 @@
-#!/bin/zsh -li
-# set -euo pipefail
-set -vx
+#!/bin/bash
 
-# Read the default ruby version out of ~/.zshrc
-eval "$(grep DEFAULT_RUBY_VERSION= ~/.zshrc)"
-
-# Install the current default Ruby version if necessary
-if [[ ! -d "$HOME/.rubies/ruby-$DEFAULT_RUBY_VERSION" ]]; then
-  chruby install "$DEFAULT_RUBY_VERSION" -- --with-openssl-dir="$(brew --prefix openssl@1.1)"
-  chruby reload
-fi
+# make sure we have the latest version of ruby installed
+ruby-install --update --cleanup --no-reinstall ruby
