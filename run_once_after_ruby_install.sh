c65efa4916101b4a448d1746a581c6b75c9f4305
André Arko <andre@arko.net>
2025-05-13 00:04:21 -0700

    [chezmoi] set bundler path to .bundle

--- run_once_after_ruby_install.sh
+++ run_once_after_ruby_install.sh
@@ -2,3 +2,6 @@
 
 # make sure we have the latest version of ruby installed
 ruby-install --update --cleanup --no-reinstall ruby
+
+# configure bundler to install gems inside each directory
+bundle config set --global path .bundle
