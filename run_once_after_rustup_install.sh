47bdfc0c82ab2935586d5beb91f9967112b7ab30
André Arko <andre@arko.net>
2025-05-27 20:27:11 -0700

    [chezmoi] install starship-jj after cargo

--- run_once_after_rustup_install.sh
+++ run_once_after_rustup_install.sh
@@ -6,3 +6,7 @@ set -vx
 if [[ ! -d ~/.cargo ]]; then
   curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path --default-toolchain nightly
 fi
+
+if [[ ! -f ~/.cargo/bin/starship-jj ]]; then
+  cargo install starship-jj --locked
+fi
