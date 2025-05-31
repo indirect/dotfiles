5a06c128fe41997e6c5338f3082eb6d3a7c732eb
André Arko <andre@arko.net>
2025-05-31 15:47:53 -0700

    [p10k] use starship-jj in p10k prompt

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -379,31 +379,7 @@
     fi
 
     if [[ -d ".jj" ]]; then
-      jj_status=$(jj log --ignore-working-copy --no-graph --color never -r @ -T '
-          separate(
-              " ",
-              bookmarks.join(", "),
-              coalesce(
-                  surround(
-                      "\"",
-                      "\"",
-                      if(
-                          description.first_line().substr(0, 24).starts_with(description.first_line()),
-                          description.first_line().substr(0, 24),
-                          description.first_line().substr(0, 23) ++ "…"
-                      )
-                  ),
-                  "(no description set)"
-              ),
-              change_id.shortest(),
-              if(conflict, "(conflict)"),
-              if(empty, "(empty)"),
-              if(divergent, "(divergent)"),
-              if(hidden, "(hidden)"),
-          )
-      ')
-      
-      typeset -g my_git_format=$jj_status
+      typeset -g my_git_format=$(starship-jj --ignore-working-copy starship prompt)
       return
     fi
 
