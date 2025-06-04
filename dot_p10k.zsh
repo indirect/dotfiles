6718b9f7767d8756a921281541c9f379780f08d7
André Arko <andre@arko.net>
2025-06-04 16:28:44 -0700

    [p10k] remove duplicate status color

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -409,7 +409,7 @@
     # If local branch name or tag is at most 32 characters long, show it in full.
     # Otherwise show the first 12 … the last 12.
     (( $#where > 32 )) && where[13,-13]="…"
-    res+="${status_color}${where//\%/%%}"  # escape %
+    res+="${where//\%/%%}"  # escape %
 
     # Display the current Git commit if there is no branch or tag.
     # Tip: To always display the current Git commit, remove `[[ -z $where ]] &&` from the next line.
